f7aabedb0959163698931e5d8b5968d08366d331
Szymon Koper <sakydpozrux@gmail.com>
2014-10-13 00:16:44 +0200

    Quick scaffold of blog with simple posts

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ActiveRecord::Base
+  validates :content, :presence => true, :length => { :minimum => 20 }
+  validates :title, :presence => true
+  validates :name, :presence => true
+end
