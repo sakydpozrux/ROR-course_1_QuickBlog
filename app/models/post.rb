f80972c0bbb6bcf1ecfc7cbb198b150801b8261f
CucumisSativus <CucumisSativus@users.noreply.github.com>
2014-10-17 16:34:16 +0200

    changed rocket to symbols
    https://github.com/bbatsov/ruby-style-guide#hash-literals

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :content, :presence => true, :length => { :minimum => 20 }
-  validates :title, :presence => true
-  validates :name, :presence => true
+  validates :content, presence: true, length: { minimum: 20 }
+  validates :title, presence: true
+  validates :name, presence: true
 end
